49c4c980ddf436b401e369e4986ce9725dda85a4
Shivam Soni <104499227+ShivamSoni1995@users.noreply.github.com>
2026-04-05 17:45:49 +0530

    Updated resume links in Resume.jsx with new Google Drive link.

---
+++ Resume.jsx
@@ -0,0 +1,5 @@
+const RESUME_VIEW_URL = "https://drive.google.com/file/d/1hWptkCrBDJArRkm8m2KQrSLwDBrP-YCU/view?usp=sharing";
+const RESUME_DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1hWptkCrBDJArRkm8m2KQrSLwDBrP-YCU";
+const RESUME_OPEN_URL = "https://drive.google.com/open?id=1hWptkCrBDJArRkm8m2KQrSLwDBrP-YCU";
+
+// Other codes remain unchanged
